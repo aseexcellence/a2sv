d3f9f2db5473277c0e2187d8ec4eb896676355d2
Asegid Shegaw <39169902+aseexcellence@users.noreply.github.com>
2024-04-18 23:16:40 +0300

    Time: 38 ms (88.09%) | Memory: 19.5 MB (17.88%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        seen = set()
+        cur = head
+        while cur:
+            if cur in seen:
+                return cur
+            seen.add(cur)
+            cur = cur.next
+        return None
+            
