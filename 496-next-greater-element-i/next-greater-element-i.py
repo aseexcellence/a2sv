35035249dcd6b003012744c6d4d7caf7f5fffb06
Asegid Shegaw <39169902+aseexcellence@users.noreply.github.com>
2024-04-24 22:08:51 +0300

    Time: 55 ms (35.97%) | Memory: 16.8 MB (87.32%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,21 +1,11 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        if not nums2:
-            return None
-
-        mapping = {}
-        result = []
         stack = []
-        stack.append(nums2[0])
-
-        for i in range(1, len(nums2)):
-            while stack and nums2[i] > stack[-1]:       
-                mapping[stack[-1]] = nums2[i]           
-                stack.pop()                             
-            stack.append(nums2[i])                      
-        for element in stack:                           
-            mapping[element] = -1
-
-        for i in range(len(nums1)):
-            result.append(mapping[nums1[i]])
-        return result
+        res = defaultdict(lambda: -1)
+        for num in nums2:
+            while stack and stack[-1] < num:
+                res[stack[-1]] = num
+                stack.pop()
+            stack.append(num)
+        
+        return [res[num] for num in nums1]
