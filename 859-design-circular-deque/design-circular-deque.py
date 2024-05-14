1ad0e8dd698e3b2ecff479ddc1d875255b174625
Asegid Shegaw <39169902+aseexcellence@users.noreply.github.com>
2024-05-14 22:53:29 +0300

    Time: 57 ms (72.80%) | Memory: 17.2 MB (68.99%) - LeetSync

---
+++ 859-design-circular-deque/design-circular-deque.py
@@ -0,0 +1,53 @@
+class MyCircularDeque:
+  def __init__(self, k: int):
+    self.queue = [None] * k
+    self.max_size = k
+    self.head = 0
+    self.tail = 0
+    self.size = 0
+
+  def insertFront(self, value: int) -> bool:
+    if self.isFull():
+        return False
+    self.head = (self.head - 1) % self.max_size
+    self.queue[self.head] = value
+    self.size += 1
+    return True
+
+  def insertLast(self, value: int) -> bool:
+    if self.isFull():
+        return False
+    self.queue[self.tail] = value
+    self.tail = (self.tail + 1) % self.max_size
+    self.size += 1
+    return True
+
+  def deleteFront(self) -> bool:
+    if self.isEmpty():
+        return False
+    self.head = (self.head + 1) % self.max_size
+    self.size -= 1
+    return True
+
+  def deleteLast(self) -> bool:
+    if self.isEmpty():
+        return False
+    self.tail = (self.tail - 1) % self.max_size
+    self.size -= 1
+    return True
+
+  def getFront(self) -> int:
+    if self.isEmpty():
+        return -1
+    return self.queue[self.head]
+
+  def getRear(self) -> int:
+    if self.isEmpty():
+        return -1
+    return self.queue[(self.tail - 1) % self.max_size]
+
+  def isEmpty(self) -> bool:
+    return self.size == 0
+
+  def isFull(self) -> bool:
+    return self.size == self.max_size
