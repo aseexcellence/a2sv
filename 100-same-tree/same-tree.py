a5c1dee17c901723a6d9c60fd9cc014908d5f55f
Asegid Shegaw <39169902+aseexcellence@users.noreply.github.com>
2024-05-16 16:25:09 +0300

    Time: 42 ms (11.40%) | Memory: 16.7 MB (10.22%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,24 +6,28 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        stack1 = []
-        stack2 = []
-        def p_recur(p: Optional[TreeNode]):
-            if p:
-                stack1.append(p.val)
-                p_recur(p.left)
-                p_recur(p.right)
-            else: stack1.append(None)
-        def q_recur(q: Optional[TreeNode]):
-            if q:
-                stack2.append(q.val)
-                q_recur(q.left)
-                q_recur(q.right)
-            else: stack2.append(None)
+        # stack1 = []
+        # stack2 = []
+        # def p_recur(p: Optional[TreeNode]):
+        #     if p:
+        #         stack1.append(p.val)
+        #         p_recur(p.left)
+        #         p_recur(p.right)
+        #     else: stack1.append(None)
+        # def q_recur(q: Optional[TreeNode]):
+        #     if q:
+        #         stack2.append(q.val)
+        #         q_recur(q.left)
+        #         q_recur(q.right)
+        #     else: stack2.append(None)
 
-        p_recur(p)
-        q_recur(q)
-        return stack1 == stack2
+        # p_recur(p)
+        # q_recur(q)
+        # return stack1 == stack2
 
+        # Recursive approach checking if each node is the sme
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
 
         
