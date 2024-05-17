45e766d9e3aa802ebeb620ddabeacc32a7bf5f64
Asegid Shegaw <39169902+aseexcellence@users.noreply.github.com>
2024-05-17 21:48:17 +0300

    Time: 57 ms (48.47%) | Memory: 19.8 MB (65.82%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        '''
+        The solution we were thought in class [A2SV] is more intuitive than
+        what I am gonna do here. However, I will get back to it later.
+        Hopefully soon!
+        '''
+        
+        if not root:
+            return root
+        
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            
+            # Finding the min from right subtree to replace the key
+            cur = root.right
+            while cur.left:
+                cur = cur.left
+            root.val = cur.val
+            root.right = self.deleteNode(root.right, root.val)
+
+        return root
+        
