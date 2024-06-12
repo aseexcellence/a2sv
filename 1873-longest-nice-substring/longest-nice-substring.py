d514d360b9fe44b25132a7a3522b3a6e5d19715f
Asegid Shegaw <39169902+aseexcellence@users.noreply.github.com>
2024-06-12 17:37:28 -0400

    Time: 36 ms (85.02%) | Memory: 16.5 MB (81.43%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.py
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -6,23 +6,18 @@ class Solution:
             hashset = set()
             for k in range(i, j + 1):
                 hashset.add(s[k])
-            # use parts array to store position (index) of delimeter in sorted order
             parts = [i - 1]
             for k in range(i, j + 1):
                 up = s[k].upper()
                 lower = s[k].lower()
-                if (up not in hashset) or (lower not in hashset): # un-paired character
+                if (up not in hashset) or (lower not in hashset):
                     parts.append(k)
             parts.append(j+1) 
-			# why do we have i - 1 and j + 1 in the array?
-			# go through this example aAbbcCEdDEfF
-			# the subproblems are:
-			# [0, 5], [7, 8], [10, 11]
             max_len_pair = (0, -1)
-			# if we don't find any delimeter, then the original string is a nice string
+
             if len(parts) == 2:
                 return (i, j)
-			# call recursively to solve each subproblem
+
             for i in range(len(parts) - 1):
                 ni, nj = helper(parts[i] + 1, parts[i+1] - 1)
                 if (nj - ni + 1) > (max_len_pair[1] - max_len_pair[0] + 1):
