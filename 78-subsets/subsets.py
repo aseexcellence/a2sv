21ad43014d7c1b5dd07e279bb8c473b80843214b
Asegid Shegaw <39169902+aseexcellence@users.noreply.github.com>
2024-06-17 17:20:15 -0400

    Time: 35 ms (72.56%) | Memory: 16.8 MB (27.39%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def backtrack(i):
+            if i == len(nums):
+                res.append(subset.copy())
+                return
+            
+            # decision to include nums[i]
+            subset.append(nums[i])
+            backtrack(i + 1)
+
+            # decision not to include nums[i]
+            subset.pop()
+            backtrack(i + 1)
+        
+        backtrack(0)
+        return res
