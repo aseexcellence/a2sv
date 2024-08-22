c9307312a516dc8ba8cdf768ed0110a16e91f9ef
Asegid Shegaw <39169902+aseexcellence@users.noreply.github.com>
2024-08-22 22:58:59 +0300

    Time: 177 ms (60.69%) | Memory: 16.9 MB (65.16%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        '''
+        Iterative O(amount * n) DP Solution
         dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
         dp[0] = [1] * (len(coins) + 1)
 
@@ -9,3 +11,18 @@ class Solution:
                 if a - coins[i] >= 0:
                     dp[a][i] += dp[a - coins[i]][i]
         return dp[amount][0]
+        '''
+
+        # Iterative O(n) solution DP Solution
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+
+            for a in range(1, amount + 1):
+                nextDP[a] = dp[a]
+                if a - coins[i] >= 0:
+                    nextDP[a] += nextDP[a - coins[i]]
+            dp = nextDP
+        return dp[amount]
